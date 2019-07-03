0ba434833495b2834bdbfb0d9b8014d5194b1146
Guilherme Storck <gstorck@ciandt.com>
2019-07-03 17:24:15 -0300

    Added liquibase to plan-catalog-service

--- plan-catalog-service/app/src/main/kotlin/com/ranefare/plancatalogservice/db/domains/InsuranceCoverageItemEntity.kt
+++ plan-catalog-service/app/src/main/kotlin/com/ranefare/plancatalogservice/db/domains/InsuranceCoverageItemEntity.kt
@@ -6,18 +6,19 @@ import javax.persistence.*
 import javax.validation.constraints.NotNull
 
 @Entity
-@Table(name = "insuranceCoverageItems")
+@Table(name = "INSURANCE_COVERAGE_ITEMS")
 data class InsuranceCoverageItemEntity(
     @Id
+    @Column(name = "ID", nullable = false, unique = true)
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
     val id: String?,
 
-    @Column
+    @Column(name = "NAME", nullable = false, length = 100)
     @NotNull
     val name: String,
 
-    @Column
+    @Column(name = "DESCRIPTION", nullable = false, length = 200)
     @NotNull
     val description: String,
 
--- plan-catalog-service/app/src/main/kotlin/com/ranefare/plancatalogservice/db/domains/InsurancePlanEntity.kt
+++ plan-catalog-service/app/src/main/kotlin/com/ranefare/plancatalogservice/db/domains/InsurancePlanEntity.kt
@@ -6,26 +6,27 @@ import javax.persistence.*
 import javax.validation.constraints.NotNull
 
 @Entity
-@Table(name = "insurancePlans")
+@Table(name = "INSURANCE_PLANS")
 data class InsurancePlanEntity(
     @Id
+    @Column(name = "ID", nullable = false, unique = true)
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
     val id: String?,
 
-    @Column
+    @Column(name = "NAME", nullable = false, length = 100)
     @NotNull
     val name: String,
 
-    @Column
+    @Column(name = "COST_RATE")
     @NotNull
     val costRate: Double,
 
     @ManyToMany(cascade = [CascadeType.MERGE])
     @JoinTable(
-        name = "insurancePlans_insuranceCoverageItems",
-        joinColumns = [JoinColumn(name = "insurancePlanId", referencedColumnName = "id")],
-        inverseJoinColumns = [JoinColumn(name = "insuranceCoverageItemId", referencedColumnName = "id")]
+        name = "INSURANCE_PLANS_INSURANCE_COVERAGE_ITEMS",
+        joinColumns = [JoinColumn(name = "INSURANCE_PLAN_ID", referencedColumnName = "ID")],
+        inverseJoinColumns = [JoinColumn(name = "INSURANCE_COVERAGE_ITEM_ID", referencedColumnName = "ID")]
     )
     val coverageItems: List<InsuranceCoverageItemEntity>
 )
